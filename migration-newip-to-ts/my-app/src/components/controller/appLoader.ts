cb57f35fae4f3889117dc4d98c36308c5c7096b4
Viktor <velenic@mail.ru>
2021-12-06 12:25:41 +0300

    fix: change api url

--- migration-newip-to-ts/my-app/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/my-app/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '6d3791bc369849d5aae9d1fd6b8b7cd6', 
         });
     }
