8bba3830496eabc6dedf540400bf7cb9af247855
Viktor <velenic@mail.ru>
2023-01-11 18:13:51 +0300

    refactor: link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi-redirect-production.up.railway.app/', {
+        super('https://newsapi-production-server.onrender.com/', {
             apiKey: '6d3791bc369849d5aae9d1fd6b8b7cd6',
         });
     }
